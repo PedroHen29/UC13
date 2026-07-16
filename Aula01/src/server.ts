bc9d1cee14f3a64c776e5c4603faf66b2d55b7d9
PEDRO HENRIQUE EICH AMARAL <05784653024@senacrs.edu.br>
2026-07-15 21:26:08 -0300

    primeira aula da UC13

--- Aula01/src/server.ts
+++ Aula01/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'; 
+import express, { Application, Request, Response, response } from 'express'; 
 
 const app: Application = express(); 
 const PORT: number = 3000; 
@@ -8,12 +8,28 @@ app.use(express.json());
 
 // Rota GET corrigida para a raiz (com a barra "/")
 app.get('/', (req: Request, res: Response): void => { 
-    res.send('🚀 Servidor TypeScript rodando!'); 
+    res.send('Servidor TypeScript rodando!'); 
 }); 
 
 app.get('/meunome', (req: Request, res: Response): void => { 
     res.send('Pedro'); 
 }); 
+
+app.get('/saudacao', (req: Request, res : Response): Response => {
+    return res.send('Olá, jovem programador!')
+})
+
+app.post('/comentarios', (req: Request, res : Response): Response => {
+    const texto = ""
+    if(texto.length == 0) return res.status(400).json({ mensagem: 'O texto não pode estar vazio!'});
+    return res.status(201).json({mensagem: 'Texto recebido com sucesso!'})
+})
+
+app.delete('comentarios/:id', (req: Request, res : Response): Response => {
+        const id = req.params.id
+        if(!id) return res.status(400).json({mensagem: 'ID está vazio!'});
+        return res.status(201).json({mensagem: 'Comentario deletado com sucesso!'})
+})
 
 // Iniciando o servidor
 app.listen(PORT, (): void => { 
