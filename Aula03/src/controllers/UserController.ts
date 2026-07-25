01616d1799a23888fda4f660842b03ab2453d4b6
PEDRO HENRIQUE EICH AMARAL <05784653024@senacrs.edu.br>
2026-07-24 21:46:38 -0300

    .

---
+++ Aula03/src/controllers/UserController.ts
@@ -0,0 +1,64 @@
+import {User, users} from "../models/User";
+import { Request, Response } from "express";
+export class UserController{
+
+    createUser(req:Request, res: Response){
+        const {name, email, age, password} = req.body
+        
+        if(!name || !email || !age || !password){
+            return res.status(400).json({message: 'name, email, age and password are necessary!'})
+        }
+
+        const id = users.length === 0 ? 1 : Math.max(users[users.length].id) + 1
+        const newUser:User = new User(id, name, email, age, password)
+        users.push(newUser)
+        return res.status(201).json({message: 'User create with success'})
+    }
+
+    getUsers(req:Request, res:Response){
+        return res.status(200).json(users)
+    }
+
+    updateUser(req:Request, res:Response){
+        const {name, email, age, password} = req.body
+        const id = Number(req.params.id)
+
+        if(!id || !name || !email || !age || !password){
+            return res.status(400).json({message: 'id, name, email, age and password are necessary!'})
+        }
+
+        const user = users.find((user) => user.id === id)
+
+        if(!user){
+            return res.status(404).json({message : 'User not found'})
+        }
+
+        user.name = name
+        user.email = email
+        user.age = age
+        user.password = password
+
+        const safeUser = {...user, password}
+
+        return res.status(200).json({
+            Message : 'User updated with success',
+            user: safeUser
+        })
+    }
+
+    deleteUser(req:Request, res:Response){
+        const id = Number(req.params.id)
+        
+        if(!id){
+            return res.status(400).json({message: 'Id are necessary!'})
+        }
+
+        const user = users.find((user) =>  user.id === id)
+
+        if(!user){
+            return res.status(404).json({message: 'User not found'})
+        }
+
+        
+    }
+}
--- Aula03/src/server.ts
+++ Aula03/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express'
 
 const app = express()
 const PORT = 3000
+// Middleware que indica que nossa API lida com JSON
+app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://loclahost:${PORT}`)
